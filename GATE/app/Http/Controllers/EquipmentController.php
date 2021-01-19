2dcebcdd5d4e89ace79490134cd79d89036dd0ae
Eduarda19 <Mariaeduardaj585@gmail.com>
2021-01-19 12:29:19 -0300

    GH-8 Criação de model, factory, controller e seed de Equipamentos

---
+++ GATE/app/Http/Controllers/EquipmentController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Equipment;
+use Illuminate\Http\Request;
+
+class EquipmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Equipment  $equipment
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Equipment $equipment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Equipment  $equipment
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Equipment $equipment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Equipment  $equipment
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Equipment $equipment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Equipment  $equipment
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Equipment $equipment)
+    {
+        //
+    }
+}
