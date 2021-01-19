d0cce275f93481ca138927ab358d5595813d5153
Maria617 <Mariac4682@gmail.com>
2021-01-19 11:48:09 -0300

    GH-7 Criação do model, migration e factory da entidade Usuário

--- GATE/app/Models/User.php
+++ GATE/app/Models/User.php
@@ -20,6 +20,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'phone',
+        'username',
+        'is_support',
     ];
 
     /**
--- GATE/database/migrations/2014_10_12_000000_create_users_table.php
+++ GATE/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,9 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->nullable();
+            $table->string('phone')->nullable();
+            $table->boolean('is_support')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
