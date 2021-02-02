38b086da6aad41361979b58855aa224509acc213
GabriellaBatista <Gabriella10batista@gmail.com>
2021-02-01 23:32:12 -0300

    GH-9 Criando relacionamento e alimentando seeder de chamados

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -8,4 +8,30 @@ use Illuminate\Database\Eloquent\Model;
 class Service extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'equipment_id',
+        'description',
+    ];
+
+    // Relationships
+    public function equipments()
+    {
+    	return $this->hasMany(Equipment::class);
+    }
+
+    public function attendances()
+    {
+    	return $this->hasMany(Attendance::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -2,6 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use App\Models\Equipment;
+use App\Models\Service;
+use App\Models\Attendance;
+use App\Models\Status;
 use Illuminate\Database\Seeder;
 
 class ServiceSeeder extends Seeder
@@ -13,6 +18,16 @@ class ServiceSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // $equipments = Equipment::select('id');
+        // $attendances = Attendance::select('id');
+        // $users = User::all();
+        $users = User::where('is_support', 0)->get();
+        foreach ($users as $user) {
+        	Service::factory(1)->create([
+        		'user_id' => $user->id,
+                'status_id' => Status::all()->random()->id,
+                'equipment_id' => Equipment::all()->random()->id,
+        	]);
+        }
     }
 }
